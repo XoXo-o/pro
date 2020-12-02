a1fd93a61c1ec82736f115f3b20e1de33b0ffe94
Юля <titarenko.julia.e@gmail.com>
2020-12-02 20:49:28 +0700

    исправление

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,7 +6,9 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Введите B:");
+            int b = Int32.Parse(Console.ReadLine());
+
         }
     }
 }
